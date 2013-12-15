44049d9b818282432abc6925f8643f70be3cbbbb
Lirrec <git@lirrec.de>
2013-12-15 02:03:31 +0100

    - Removed Submodules - reworked CMakeLists.txt, dll creation is now possible. - added a simple FindSBE.cmake script

---
+++ cmake/FindSBE.cmake
@@ -0,0 +1,119 @@
+# This script locates the SBE library.
+#
+# USAGE
+#
+# By default, the dynamic version of SBE will be found. To find the static
+# one instead, you must set the SBE_STATIC_LIBRARIES variable to TRUE before
+# calling find_package( SBE ). In that case SBE_STATIC will also be defined
+# by this script. Example:
+#
+# set( SBE_STATIC_LIBRARIES TRUE )
+# find_package( SBE )
+#
+# If SBE is not installed in a standard path, you can use the SBEDIR or
+# SBE_ROOT CMake (or environment) variables to tell CMake where to look for
+# SBE.
+#
+#
+# OUTPUT
+#
+# This script defines the following variables:
+#   - SBE_LIBRARY_DEBUG:   the path to the debug library
+#   - SBE_LIBRARY_RELEASE: the path to the release library
+#   - SBE_LIBRARY:         the path to the library to link to
+#   - SBE_FOUND:           true if the SBE library is found
+#   - SBE_INCLUDE_DIR:     the path where SBE headers are located (the directory containing the SBE/Config.hpp file)
+#
+#
+# EXAMPLE
+#
+# find_package( SBE REQUIRED )
+# include_directories( ${SBE_INCLUDE_DIR} )
+# add_executable( myapp ... )
+# target_link_libraries( myapp ${SBE_LIBRARY} ... )
+
+set( SBE_FOUND false )
+
+if( SBE_STATIC_LIBRARIES )
+	set( SBE_SUFFIX "" )
+	add_definitions( -DSBE_STATIC )
+else()
+	set( SBE_SUFFIX "" )
+endif()
+
+message( "sberoot ${SBE_ROOT}")
+
+set( FIND_SBE_PATHS
+		${SBEDIR}
+		${SBE_ROOT}
+		$ENV{SBEDIR}
+		$ENV{SBE_ROOT})
+
+message( "FIND_SBE_PATHS ${FIND_SBE_PATHS}" )
+		
+find_path(
+	SBE_INCLUDE_DIR
+	sbe/Engine.hpp
+	PATH_SUFFIXES
+		include
+	PATHS
+		${FIND_SBE_PATHS}
+	NO_DEFAULT_PATH
+)
+
+find_library(
+	SBE_LIBRARY_RELEASE
+	SBE${SBE_SUFFIX}
+	PATH_SUFFIXES
+		lib
+		lib64
+		build
+		build.win
+	PATHS
+		${FIND_SBE_PATHS}
+	NO_DEFAULT_PATH
+)
+
+find_library(
+	SBE_LIBRARY_DEBUG
+	SBE${SBE_SUFFIX}-d
+	PATH_SUFFIXES
+		lib
+		lib64
+		build
+		build.win
+	PATHS
+		${FIND_SBE_PATHS}
+	NO_DEFAULT_PATH
+)
+
+message( "sberls ${SBE_LIBRARY_RELEASE}")
+message( "sbedbg ${SBE_LIBRARY_DEBUG}")
+message( "sbeinc ${SBE_INCLUDE_DIR}")
+
+if( SBE_LIBRARY_RELEASE AND SBE_LIBRARY_DEBUG )
+	set( SBE_LIBRARY debug ${SBE_LIBRARY_DEBUG} optimized ${SBE_LIBRARY_RELEASE} )
+endif()
+
+if( SBE_LIBRARY_RELEASE AND NOT SBE_LIBRARY_DEBUG )
+	set( SBE_LIBRARY_DEBUG ${SBE_LIBRARY_RELEASE} )
+	set( SBE_LIBRARY ${SBE_LIBRARY_RELEASE} )
+endif()
+
+if( SBE_LIBRARY_DEBUG AND NOT SBE_LIBRARY_RELEASE )
+	set( SBE_LIBRARY_RELEASE ${SBE_LIBRARY_DEBUG} )
+	set( SBE_LIBRARY ${SBE_LIBRARY_DEBUG} )
+endif()
+
+if( NOT SBE_INCLUDE_DIR OR NOT SBE_LIBRARY )
+	if( SBE_FIND_REQUIRED )
+		message( FATAL_ERROR "SBE not found." )
+	elseif( NOT SBE_FIND_QUIETLY )
+		message( "SBE not found." )
+	endif()
+else()
+	set( SBE_FOUND true )
+	if ( NOT SBE_FIND_QUIETLY )
+		message( STATUS "SBE found: ${SBE_INCLUDE_DIR} with ${SBE_LIBRARY}" )
+	endif()
+endif()
