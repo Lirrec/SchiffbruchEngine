Lirrec/SchiffbruchEngine

commit 9a11ebf44f7f999a1d4ea12aa309dded9c245b9c

- IO::loadPath is now able to load absolute paths

src/io/IO.ipp
@@ -48,6 +48,10 @@ namespace sbe
 				}
 			}
 
+			tmp = loadPath<T>( IOP, "", filename);
+			if(!tmp.empty()){ found = true; }
+
+
 		}
 		catch (fs::filesystem_error& e)
 		{
@@ -63,27 +67,34 @@ namespace sbe
 	template<class T>
 	std::vector<std::shared_ptr<T>> IO::loadPath(  std::shared_ptr<IOPlugin>& IOP, const std::string& current_path, const std::string& filename)
 	{
-	    Engine::out() << "currentpath: " << current_path << std::endl;
-	    Engine::out() << "filename: " << filename << std::endl;
+	    //Engine::out() << "currentpath: " << current_path << std::endl;
+	    //Engine::out() << "filename: " << filename << std::endl;
 
 		fs::path cp(current_path);
+		fs::path filepath( filename );
 
-		if ( !fs::exists( cp ) )
+		if ( filepath.is_absolute() )
 		{
-			Engine::out(Engine::ERROR) << "[IO] Unable to load path! '" << cp.generic_string() << "' not found!" << std::endl;
-			return std::vector<std::shared_ptr<T>>();
+			cp = filepath;
+		}
+		else
+		{
+			if ( !fs::exists( cp ) )
+			{
+				Engine::out(Engine::ERROR) << "[IO] Unable to load path! '" << cp.generic_string() << "' not found!" << std::endl;
+				return std::vector<std::shared_ptr<T>>();
+			}
+
+			if ( fs::is_regular_file(cp / filename)  ) return loadFile<T>( IOP , cp / filename );
+			cp /= IOP->relative_path;
+			cp /= filename;
 		}
 
 
-		// check if we got a valid file
-		if ( fs::is_regular_file(cp / filename)  ) return loadFile<T>( IOP , cp / filename );
 
-		cp /= IOP->relative_path;
-		cp /= filename;
-
-		if ( fs::is_regular_file(cp)  ) return loadFile<T>( IOP , cp );
-
-		if ( fs::is_directory(cp) )
+		if ( fs::is_regular_file(cp)  )
+			return loadFile<T>( IOP , cp );
+		else if ( fs::is_directory(cp) )
 		{
 			if (IOP->loader_type == IOPlugin::loader::PTREE )
 			{

commit a93ed93bba722af9b7e21568968775db325518db

- fixed a crash in IO.ipp introduced in 9a11ebf44f7f999a1d4ea12aa309dded9c245b9c

src/io/IO.ipp
@@ -47,11 +47,12 @@ namespace sbe
 					break;
 				}
 			}
-
-			tmp = loadPath<T>( IOP, "", filename);
-			if(!tmp.empty()){ found = true; }
-
-
+			if ( !found )
+			{
+				// try an absolute path
+				tmp = loadPath<T>( IOP, "", filename);
+				if(!tmp.empty()){ found = true; }
+			}
 		}
 		catch (fs::filesystem_error& e)
 		{
@@ -61,6 +62,7 @@ namespace sbe
 		if(found){
 			return tmp;
 		}
+
 		return std::vector<std::shared_ptr<T>>();
 	}
 
@@ -79,15 +81,15 @@ namespace sbe
 		}
 		else
 		{
-			if ( !fs::exists( cp ) )
-			{
-				Engine::out(Engine::ERROR) << "[IO] Unable to load path! '" << cp.generic_string() << "' not found!" << std::endl;
-				return std::vector<std::shared_ptr<T>>();
-			}
-
 			if ( fs::is_regular_file(cp / filename)  ) return loadFile<T>( IOP , cp / filename );
 			cp /= IOP->relative_path;
 			cp /= filename;
+
+			if ( !fs::exists( cp ) )
+			{
+				//Engine::out(Engine::ERROR) << "[IO] Unable to load path! '" << cp.generic_string() << "' not found!" << std::endl;
+				return std::vector<std::shared_ptr<T>>();
+			}
 		}
 
 